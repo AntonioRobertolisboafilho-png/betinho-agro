457116992ca5090ddc1cb1b3b317ff2fc1d86de2
AntonioRobertolisboafilho-png <antoniorobertolisboafilho@gmail.com>
2026-07-14 17:15:48 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='bl-suinos-v5-0';
+const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-bl-suinos.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))));
